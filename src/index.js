f78773dd70e698641d7b30cd337de98683408538
Kseniia Beliaeva <ksenia.belyaewa@gmail.com>
2018-01-28 12:31:52 +1100

    JSX Expressions

--- src/index.js
+++ src/index.js
@@ -2,9 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const tasks = ['take out the trash', 'shovel the driveway', 'walk the dog'];
-const element = React.createElement('ol', null,
-    tasks.map((task, index) => React.createElement('li', { key: index }, task))
-);
+const element =
+    <div>
+        <h1>Task list</h1>
+      <ol>
+       {tasks.map((task, index) => <li key={index}> {task} </li>)} {/*createElement method*/}
+    {/* <li>{tasks[0]}</li>  {/* whenever we want to write javascript expressions and include them in these tags they must be enclosed by {}
+    <li>{tasks[1]}</li>
+    <li>{tasks[2]}</li> */}
+      </ol>
+    </div>    
+
 ReactDOM.render(element, document.getElementById('root'));
 
 
