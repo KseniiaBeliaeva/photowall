556690e2ef55eaacdebafb12cb106e5732872b3b
Kseniia Beliaeva <ksenia.belyaewa@gmail.com>
2018-01-28 12:15:48 +1100

    React elements exersice

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const tasks = ['take out the trash', 'shovel the driveway', 'walk the dog'];
+const element = React.createElement('ol', null,
+    tasks.map((task, index) => React.createElement('li', { key: index }, task))
+);
+ReactDOM.render(element, document.getElementById('root'));
+
+
