e8be068b9b11f9b00105794965c37428b1165df3
Kseniia Beliaeva <ksenia.belyaewa@gmail.com>
2018-01-29 11:36:04 +1100

    React Components ex

--- src/index.js
+++ src/index.js
@@ -1,18 +1,36 @@
-import React from "react";
+import React, {Component} from "react";
 import ReactDOM from "react-dom";
 
-const tasks = ['take out the trash', 'shovel the driveway', 'walk the dog'];
-const element =
-    <div>
-        <h1>Task list</h1>
-      <ol>
-       {tasks.map((task, index) => <li key={index}> {task} </li>)} {/*createElement method*/}
-    {/* <li>{tasks[0]}</li>  {/* whenever we want to write javascript expressions and include them in these tags they must be enclosed by {}
-    <li>{tasks[1]}</li>
-    <li>{tasks[2]}</li> */}
-      </ol>
-    </div>    
 
-ReactDOM.render(element, document.getElementById('root'));
+
+
+class List extends Component{ 
+    render() {
+        return (
+            <ol>
+                {this.props.tasks.map((task, index) => <li key={index}> {task} </li>)}
+            </ol>
+    )
+}
+}
+class Title extends Component {
+    render() {
+        return <h1>{this.props.todo}</h1>
+    }
+}
+
+class Main extends Component {
+    render() { 
+        return (
+        <div>
+                <Title todo={"toDos"}/>
+                <List tasks={['Mow the lawn','walk the dog']}/>
+                <List tasks={['Hose the driveway','finish the laundry']}/>    
+
+            </div>
+        )    
+    }
+}
+ReactDOM.render(<Main/>, document.getElementById('root'));
 
 
